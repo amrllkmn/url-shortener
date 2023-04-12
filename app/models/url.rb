ba9e0adac7288cb20cb572a5f9c16eebe36c3d07
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-04-12 22:14:33 +0800

    Auto-generate slugs

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,12 @@
 class Url < ApplicationRecord
+    before_validation :generate_slug
     validates_presence_of :target_url
-    validates_uniqueness_of :short_url
-
-    def shorten_url(original_url)
+    validates :target_url, format: URI::regexp(%w[http https])
+    validates_uniqueness_of :slug
+  
+    def generate_slug
+        self.slug = SecureRandom.uuid[0..5] if self.slug.nil? || self.slug.empty?
+        true
     end
 
 end
