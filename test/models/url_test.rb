7239365cb16df8f6029cec365ddb10f3fe864ad7
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-04-13 20:57:51 +0800

    Use more readable assertion

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -7,61 +7,68 @@ class UrlTest < ActiveSupport::TestCase
 
   test "Creating a URL with no slug should create a slug" do
     url = Url.create(target_url: "https://google.com", slug:"")
-    assert !url.slug.empty?
+    assert_not_empty url.slug
   end
 
   test "Creating a URL with a slug should return same slug" do
     test_slug = "testing"
     url = Url.create(target_url: "https://google.com", slug: test_slug)
-    assert url.slug == test_slug
+    assert_equal(url.slug, test_slug)
   end
 
   test "Creating 2 URLs with same slug should not be created" do
     url_1_slug = Url.find(1).slug
     url_2 = Url.create(target_url: "https://google.com", slug: url_1_slug)
-    assert url_2.id.nil?
+    assert_nil url_2.id
   end
 
   test "Creating URL without https should not be created" do
     url = Url.create(target_url: "www.google.com")
-    assert url.id.nil?
+    assert_nil url.id
   end
 
 
   test "shorten_url should return a string url" do
     url = Url.shorten_url("https://google.com","","test.com")
-    assert !url.empty?
+    assert_not_empty url
   end
 
   test "shorten_url should return url with existing slug" do
     url_1_slug = Url.find(1).slug
+    new_url = Url.shorten_url("https://google.com", url_1_slug, "test.com")
+    new_slug = new_url.split("/")[-1]
+    assert_equal(Url.find_by(slug: new_slug).target_url,"https://google.com")
+  end
+
+  test "shorten_url should add character if with non-unique slug" do
+    url_1_slug = Url.find(1).slug
     new_url = Url.shorten_url("https://youtube.com", url_1_slug, "test.com")
     new_slug = new_url.split("/")[-1]
-    assert Url.find_by(slug: new_slug).target_url == "https://google.com"
+    assert_not_equal(new_slug, url_1_slug)
   end
 
   test "update_url should increment clicks and click_timestamp and origin" do
     url_1 = Url.find(1)
     geolocation = {"city": "Kuala Lumpur", "region": "Kuala Lumpur", "country": "Malaysia"}
     updated_url = Url.update_url(url_1.slug, geolocation)
-    assert updated_url.times_clicked == 2
-    assert JSON.parse(updated_url.origin).length == 2
-    assert JSON.parse(updated_url.click_timestamp).keys.length == 2
+    assert_equal(updated_url.times_clicked, 2)
+    assert_equal(JSON.parse(updated_url.origin).length, 2)
+    assert_equal(JSON.parse(updated_url.click_timestamp).keys.length, 2)
   end
 
   test "update_url should return nil if not found" do
     geolocation = {"city": "Kuala Lumpur", "region": "Kuala Lumpur", "country": "Malaysia"}
     updated_url = Url.update_url("non_existing", geolocation)
-    assert updated_url.nil?
+    assert_nil updated_url
   end
 
   test "get_report should return data if url found" do
     report = Url.get_report(1) # get Url with ID 1
-    assert !report.nil?
+    assert_not_nil report
   end
 
   test "get_report should return nil if url not found" do
     report = Url.get_report(5) # get Url with ID 5 which don't exist
-    assert report.nil?
+    assert_nil report
   end
 end
